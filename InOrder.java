f4cd2cfe8935511b7152c27e43affb430c37bf1f
Michal Barak <michal.barak01@post.runi.ac.il>
2024-01-01 20:23:56 +0200

    submitted code for Q3

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,18 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		/// Generate a number in range [0,10) and generate a boolean to check if decreasing order value is met
+		int num = (int) (Math.random() * 10);
+		boolean isDecreasing = false; 
+		/// As long as num - new_num =! 1, the loop will generate a new number and check if criteria is met
+		do { 
+			int new_num = (int) (Math.random() * 10);
+			System.out.print(num + " ");
+			if (num - new_num == 1) {
+				isDecreasing = true;
+			}
+			num = new_num;
+		}
+		while (isDecreasing == false);  
 	}
 }
