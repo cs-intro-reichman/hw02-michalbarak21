c45d9c8eb41bda06a76298dc22f665dce1192fe0
Michal Barak <michal.barak01@post.runi.ac.il>
2024-01-02 15:29:23 +0200

    Submitted code for Q5

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,19 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		/// Gets input from user
+		int num = Integer.parseInt(args[0]);
+		/// Creates a line in the damka board based on the number given by user
+		String base = ( "* "); 
+		String line = base.repeat(num); 
+		for (int i = 1; i <= num; i++) {
+			/// For each row checks if it's even or odd. 
+			/// odd --> prints line as is; even --> adds space infront of the line to create indentation  
+			if ( i % 2 == 1) {
+				System.out.println(line);
+			} else { 
+				System.err.println(" " + line);
+			}
+		}
 	}
 }
