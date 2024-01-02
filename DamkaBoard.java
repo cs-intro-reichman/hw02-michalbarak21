fbd226ee1f930d4f108585aad659fec5a1af729b
Michal Barak <michal.barak01@post.runi.ac.il>
2024-01-02 15:41:33 +0200

    Changed printing of Q5 for autograding

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,15 +6,16 @@ public class DamkaBoard {
 		/// Gets input from user
 		int num = Integer.parseInt(args[0]);
 		/// Creates a line in the damka board based on the number given by user
-		String base = ( "* "); 
-		String line = base.repeat(num); 
+		String base = ( " *"); 
+		String line_even = (" *") + base.repeat(num - 1); 
+		String line_odd = ("*" + base.repeat(num - 1));
 		for (int i = 1; i <= num; i++) {
 			/// For each row checks if it's even or odd. 
 			/// odd --> prints line as is; even --> adds space infront of the line to create indentation  
 			if ( i % 2 == 1) {
-				System.out.println(line);
+				System.out.println(line_odd);
 			} else { 
-				System.out.println(" " + line);
+				System.out.println(line_even);
 			}
 		}
 	}
