4ffd253143be155b8b7233ba388329e0fa47b96b
Michal Barak <michal.barak01@post.runi.ac.il>
2024-01-02 16:04:51 +0200

    Submitted code for Q6

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,24 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		//// Generate a boolean argument to follow if the family has a boy / girl and a count integer
+		boolean girl = false; 
+		boolean boy = false;
+		int count = 0; 
+		while (girl == false || boy == false) {
+			/// Generate a random number between [0, 1)
+			/// values greater than 0.5 are considered girls, lower are considered boys
+			double number = Math.random(); 
+			if (number > 0.5) { 
+				girl = true; 
+				System.out.print("g ");
+			} else { 
+				boy = true; 
+				System.out.print("b ");
+			}
+			count++;
+		}
+		System.out.println();
+		System.out.print("You made it... and you now have " + count + " children.");
 	}
 }
